7c6dd551b4569350decd9beff3eb8f907c149659
Nicholas N <nnlaor@gmail.com>
2017-04-21 13:45:20 -0700

    including connection and sudo code for functions needed

--- app.js
+++ app.js
@@ -1,69 +1,54 @@
 var mysql = require("mysql");
-// questions
+var conetable = require("console.table");
 
-// did you put functions in different files
-// all in one sheet
+
 // sudo code
-// how to display the mysql data for the use tosee 
 
-// data base credentials
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "Bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "Bamazon"
 });
 
-// data base connection
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
 
-
-
-// 3
-// create a table products
-	// columbs 
-	// item_id
-	// product_name 
-	// department_name
-	// price cost
-	// stock_quantity
-
-// CREATE TABLE products (
-//  id INT(255) NOT NULL,
-//  product_name VARCHAR(100) NULL,
-//  department_name VARCHAR(100) NULL,
-//  price DECIMAL(10,4) NULL,
-//  stock_quantity INT(1000) NULL,
-//  PRIMARY KEY (id)
-// );
 
 
 
 // 4
-// populate the data base with 10 items
+	// populate the data base with 10 items
+
 
 // 5
-// create bamazonCustomer.js
-// npm start
-// available for sale. Include the ids, names, 
-// and prices of products for sale.
+	// create bamazonCustomer.js
+	// npm start
+	// available for sale. Include the ids, names, 
+	// and prices of products for sale.
+	// display inventory function
+	// start to shop
+
+
 
 
 
 // 6 
-// ask the id of the item
-// ask the quantity
+	// ask the id of the item
+	// ask the quantity	
+	// shoping mode
+
 
 // 7
-// check the quantity of items before continuing the trasaction
+	// check the quantity of items before continuing the trasaction
+	// inventory check function
+	// data validation ? inquirer
+
 
 // 8
-// update the mysql database
+	// update the mysql database
+	// update inventory
+
